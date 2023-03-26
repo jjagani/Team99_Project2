cac40c36166e4df3ffdb6732d767d5c692bc701e
Isaac Morton <isaacsmorton@gmail.com>
2023-03-26 00:18:03 -0400

    Particle Initial Conditions Producer
    inputs width and outputs vector of necessary particle IC information.
    
    also histogram testing to understand parabolic distributions.
    
    see comments for more details.

--- particleIC.m
+++ particleIC.m
@@ -1,10 +1,25 @@
 function [posIC, velIC, accIC] = particleIC(width)
 % Outputs the necessary initial conditions for one particle
-%   Position in the x direction is normally randomized based on laminar
+%   Position in the x direction is parabolically randomized based on laminar
 %   flow. Velocity in the x direction is zero to start. Acceleration in the
 %   x direction is zero to start.
-posIC = normrnd(width / 2, %standard deviation goes here)
-posIC = ;
+
+% this finds a random float between 1 and 0 and then .5 is subtracted 
+% from it
+rawValue = rand(1,1) - .5;
+
+% retains the sign of the value after squaring
+if rawValue > 0 
+    squareValue = rawValue^2;
+elseif rawValue <= 0
+    squareValue = -1 * (rawValue^2);
+end
+
+% applies the random number to the width of the plates
+appliedValue = width * (2 * squareValue + .5);
+
+posIC = appliedValue;
 velIC = 0;
 accIC = 0;
+
 end
