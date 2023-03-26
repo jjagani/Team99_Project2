9cc02360a45227248d44f6e000b4330a633bf0b5
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2023-03-26 16:15:32 -0400

    asdf

--- odefunc.m
+++ odefunc.m
@@ -3,21 +3,25 @@ function dxdt = odefunc(t, x)
 %x : [x1, x2] x1 = position x, x2 = velocity x
 %d : diameter of the particle (Assuming spherical particle) (mm)
 
-p = 10; %density of particle (g/cm^3)
-epsilon = 8.85e-12; %Permittivity of Free Space (Assumed as permittivity of air)
-q = 1.602e-19; %Charge of Particles
-w = 0.05; %distance of plates from each other (m)
-l = 1; %length of plate (m)
-h = 2; %height of plate (m)
-V = 10000; %Voltage on Plate (Volts)
-c = 21; %concentration of particles (ug/m^3)
-mu = 1.81e-5; %dynamic viscosity of air
-d = 5e-6; %PE
+p_part = 1;
+d_part = 1;
+a_vert = 1;
+p_fluid = 1;
+g = 1;
+c_d = 1;
+v_vert = 1;
+q = 1;
+sigma = 1;
+epsilon = 1;
+c_t = 1;
+H = 1;
+d_vert = 1;
 
-p = p * 1e-3;
-c = c * 1e-9;
-mp = (pi / 6) * p * d ^ 3; 
+
+
+
 dxdt(1) = x(2); 
-dxdt(2) = 1/mp * ( q^2 * c / (2 * epsilon) * (2*x(1) - w) + (q *  V) / (4 * pi * w) - 3*pi*mu*d * x(2));
+dxdt(2) = (pi/2 * (p_fluid- p_part)*g*d_part^3+1/2*p_fluid*c_d*(pi/4*d_part^2)*v_vert^2 - q*sigma/(2*pi*epsilon) + q^2*c_t/(2*epsilon)*(2*d_vert-H)) / (pi/6*p_part*d_part^3);
+%1/mp * ( q^2 * c / (2 * epsilon) * (2*x(1) - w) + (q *  V) / (4 * pi * w) - 3*pi*mu*d * x(2));
 dxdt = transpose(dxdt);
 end
