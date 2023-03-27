4043aa4d582b1e36ae39590fb866dc6e1acb0754
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2023-03-27 17:18:12 -0400

    asdf

--- simParticle.m
+++ simParticle.m
@@ -1,27 +1,27 @@
-function [tDiscretized, h_position, h_velocity, v_position] = simParticle(particleData, intialCond, simData)
+function [tDiscretized, h_position, h_velocity, v_position] = simParticle(particleData, initialCond, tDiscretized)
 %simParticle simulates one particle given its parameters
 
 %% ======= DATA SETUP =======
 %% particle data that does not change
 % must be input in this order
-p_particle =  particleData(1)
-epsilon =     particleData(2)
-q_particle =  particleData(3)
-spacing =     particleData(4)
-V =           particleData(5)
-conc =        particleData(6)
-mu =          particleData(7)
-diam =        particleData(8)
+p_particle =  particleData(1);
+epsilon =     particleData(2);
+q_particle =  particleData(3);
+spacing =     particleData(4);
+V =           particleData(5);
+conc =        particleData(6);
+mu =          particleData(7);
+diam =        particleData(8);
+maxAirspeed = particleData(9);
 
 %% intial conditions of the particle
 % of the form [position velocity]
 
 %% data for running the simulation
-tDiscretized  =  simData(1);
-outputSteps = simData(2); % number of steps of position to output to save memory
+%outputSteps; % number of steps of position to output to save memory
 
 %% Run simulation
-[~,soln] = ode45(@(t,x) odefunc(t,x,p_particle, epsilon, q_particle, spacing, V, conc, mu, diam), tDiscretized, intialCond);
+[t,soln] = ode45(@(t,x) odefunc(t,x,p_particle, epsilon, q_particle, spacing, V, conc, mu, diam), tDiscretized, initialCond);
     
 %% process results
 % cutoff data when it hits the plate (or where the plate would be)
@@ -34,7 +34,7 @@ tDiscretized = tDiscretized(1:cutoff);
 % calculate vertical position
 v_position = zeros(1, cutoff);
 for i = 2:cutoff
-    v_position(i) = v_position(i-1) + particleSpeed(h_position(i), spacing, 0, 1) * (tDiscretized(i) - tDiscretized(i-1));
+    v_position(i) = v_position(i-1) + particleSpeed(h_position(i), spacing, 0, maxAirspeed) * (tDiscretized(i) - tDiscretized(i-1));
 end
 
 
