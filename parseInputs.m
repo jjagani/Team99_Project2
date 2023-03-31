96aa6ed9bd0a1302023d166097e8f82945c5ffac
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2023-03-31 02:51:53 -0400

    asd

--- parseInputs.m
+++ parseInputs.m
@@ -1,7 +1,7 @@
 function [particleDataMat, initialCondMat] = parseInputs(userInput)
-%PARSEINPUTS Summary of this function goes here
-%   Detailed explanation goes here
-
+%% == Hardcoded Values ==
+%% each variable is of the form [start end number of values]
+%% (so to keep it constant, do x = [constant, constant, 1])
 % density of particle (g/cm^3) (Avg density of PM2.5 and PM10 particles)
 p_particle =  [2 2 1];
 
@@ -30,7 +30,7 @@ diam =        [5 5 1];
 maxAirspeed = [5 5 1]; 
 
 % Initial position of the particle (m)
-initialP =    [0 .1 4];
+initialP =    [0 .09 10];
 
 % Initial velocity of the particle (m/s)
 initialV =    [0 0 1];
@@ -43,6 +43,9 @@ parsedDataMat = 0;
 
 rawInputs = [p_particle; epsilon; q_particle; spacing; V; conc; mu; diam; maxAirspeed; initialP; initialV];
 
+%% == expand parsedDataMat ==
+% turn it into a matrix where each row represents one particle, and there
+% are engough rows to cover every possible combination of the inputs
 for i=1:length(rawInputs)
     rawSingleInput = rawInputs(i,:);
     spacedVals = linspace(rawSingleInput(1), rawSingleInput(2), rawSingleInput(3));
@@ -52,6 +55,7 @@ for i=1:length(rawInputs)
     parsedDataMat(:,i) = repSpacedVals;
 end
 
+% return values
 particleDataMat = parsedDataMat(:,1:9);
 initialCondMat = parsedDataMat(:,10:11);
 end
