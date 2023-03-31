4b74c68b3983d62c1c4be2869a4ff5c0aab5ff61
LoneSurvivr314 <73797060+LoneSurvivr314@users.noreply.github.com>
2023-03-31 02:10:36 -0400

    Update parseInputs.m

--- parseInputs.m
+++ parseInputs.m
@@ -2,23 +2,44 @@ function [particleDataMat, initialCondMat] = parseInputs(userInput)
 %PARSEINPUTS Summary of this function goes here
 %   Detailed explanation goes here
 
-p_particle =  2;              % density of particle (g/cm^3) (Avg density of PM2.5 and PM10 particles)
-epsilon =     8.85e-12;       % Permittivity of Free Space (Assumed as permittivity of air)
-q_particle =  1.602e-19;      % Charge of Particles (Assume only one electron attaches)
-spacing =     0.1;            % distance of plates from each other (m)
-V =           176;            % Voltage on Plate (Volts)
-conc =        75;             % concentration of particles (ug/m^3)
-mu =          1.81e-5;        % dynamic viscosity of air
-diam =        5;              % Diameter of Particle (um)
-maxAirspeed = 5;              % Maximum airspeed in center (m/s)
+% density of particle (g/cm^3) (Avg density of PM2.5 and PM10 particles)
+p_particle =  [2 2 1];
 
-initialP =    0;              % Initial position of the particle (m)
-initialV =    0;              % Initial velocity of the particle (m)
+% Permittivity of Free Space (Assumed as permittivity of air)
+epsilon =     [8.85e-12 8.85e-12 1];
+
+% Charge of Particles (Assume only one electron attaches)
+q_particle =  [1.602e-19 1.602e-19 1];
+
+% distance of plates from each other (m)
+spacing =     [0.1 0.1 1];
+
+% Voltage on Plate (Volts)
+V =           [176 176 1];
+
+% concentration of particles (ug/m^3)
+conc =        [75 75 1];
+
+% dynamic viscosity of air
+mu =          [1.8e-5 1.81e-5 1];
+
+% Diameter of Particle (um)
+diam =        [5 5 1];
+
+% Maximum airspeed in center (m/s)
+maxAirspeed = [5 5 1]; 
+
+% Initial position of the particle (m)
+initialP =    [0 0 1];
+
+% Initial velocity of the particle (m/s)
+initialV =    [0 0 1];
 
 if(userInput)
     % get user input here
 end
-
+allRawData = [p_particle; epsilon; q_particle; spacing; conc; mu; diam; maxAirspeed; initialP; initialV];
+allRawData
 
 
 end
